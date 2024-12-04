4448deccb84a6eb68feff89e12806c3768df5fe7
Oláh Katalin <okatalin@gmail.com>
2024-12-04 09:55:24 +0100

    emoji generator base

---
+++ src/main.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('button');
+
+//emoji generálása a Generate gomb kattintás eseményére
+btn.addEventListener('click', () => {        
+   
+});
+
+// rnd függvény 
+
